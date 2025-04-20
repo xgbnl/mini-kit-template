e55cefe76f5db8b26b7168bdb5d531ba0c015a74
xgbnl <745502274@qq.com>
2025-04-20 14:36:51 +0800

    chore: 优化代码

--- utils/dispatchTabBar.ts
+++ utils/dispatchTabBar.ts
@@ -1,7 +1,12 @@
-export function dispatchTabBar<PageData>({ page, state: payload }: { page: MiniProgram.Page.IInstance<PageData, any, any>, state: Record<string, number> }) {
+type Params<PageData> = {
+  page: MiniProgram.Page.IInstance<PageData, any, any>,
+  state: Record<string, number>
+}
+
+export function dispatchTabBar<PageData>({ page, state }: Params<PageData>) {
 
   if (typeof page.getTabBar === 'function' && page.getTabBar()) {
-    page.getTabBar().setData(payload)
+    page.getTabBar().setData(state)
   }
 
 }
